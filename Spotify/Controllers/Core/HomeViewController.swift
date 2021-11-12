65af5a1808de790626f03c677ab11f6344c7b5a7
matteoKuama <matteo@kuama.net>
2021-11-12 10:03:51 +0100

    Launchscreen

--- Spotify/Controllers/Core/HomeViewController.swift
+++ Spotify/Controllers/Core/HomeViewController.swift
@@ -13,10 +13,11 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         title = "Home"
         view.backgroundColor = .systemBackground
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"),
-                                                             style: .done,
-                                                             target: self,
-                                                             action: #selector(didTapSettings))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "gear"),
+            style: .done,
+            target: self,
+            action: #selector(didTapSettings))
         fetchData()
     }
     
